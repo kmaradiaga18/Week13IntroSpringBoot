fbf9f623b21fbcf7324af696ae884e6299d7a3e4
Karen Maradiaga <kmaradiaga18@gmail.com>
2022-12-17 22:18:55 -0600

    Creating API SpringBoot

---
+++ src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -0,0 +1,17 @@
+package com.promineotech.jeep.entity;
+
+import java.math.BigDecimal;
+
+public class Jeep {
+  
+    private Long modelPK;
+    private JeepModel modelId;
+    private String trimLevel;
+    private int numDoors;
+    private int wheelSize;
+    private BigDecimal basePrice;
+  }
+
+
+ 
+
